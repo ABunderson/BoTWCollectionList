141176577af27daf8e30c60bec9858db851708fe
Adrienne Bunderson <bun18015@byui.edu>
2023-07-15 12:43:10 -0600

    fix custom location breaking sorts

--- src/js/list.js
+++ src/js/list.js
@@ -16,3 +16,20 @@ document.forms['custom-item'].addEventListener('submit', (e) => {
     addCustomToList(e.target);
     window.location = '/list/index.html';
 });
+
+if (document.querySelector('.collection-list')) {
+    document.querySelector('#list-name-sort').addEventListener('click', () => {
+        console.log('name sort')
+        renderCollectionList('nameSort');
+    })
+
+    document.querySelector('#list-date-sort').addEventListener('click', () => {
+        console.log('date sort')
+        renderCollectionList('dateSort');
+    })
+
+    document.querySelector('#list-quantity-sort').addEventListener('click', () => {
+        console.log('quan sort')
+        renderCollectionList('quantitySort');
+    })
+}
--- src/js/list.mjs
+++ src/js/list.mjs
@@ -6,6 +6,7 @@ let sortVariable;
 export function renderCollectionList(sort) {
 
     const list = getLocalStorage('list');
+    console.log(sort)
 
     if (list === null) {
         // console.log('list null')
@@ -20,7 +21,6 @@ export function renderCollectionList(sort) {
         return;
     }
 
-    listSortSelectors()
 
     // sort here
     list.sort((p1, p2) => (p1.date > p2.date) ? 1 : (p1.date < p2.date) ? -1 : 0);
